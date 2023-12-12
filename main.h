50d5eab3651378bef0e2acdc6cc7ee16b1bea75f
akoredera <akoredera@gmail.com>
2023-12-12 14:49:21 -0800

    add: main.h file

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef main_H
+#define main_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+extern char **environ;
+char **tokenize(char *str, const char *delim);
+void pro_exec(char **commands);
+void free_commands(char **command);
+void get_pathname(char **command);
+void exit_func(char **tok_ret, size_t command_count);
+void env_func(char **tok_ret, size_t command_count);
+#endif
