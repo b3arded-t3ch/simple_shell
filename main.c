a2836a1a3fecaa81c7603b98b24c1eedeea82db0
akoredera <akoredera@gmail.com>
2023-12-13 02:30:14 -0800

    modified main.c file

--- main.c
+++ main.c
@@ -6,25 +6,10 @@
 int main(void)
 {
 	char *input = NULL, **tok_ret = NULL;
-	size_t size = 0, command_count = 0;
-	ssize_t ret_num;
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))
-			printf("$ ");
-		fflush(stdout);
-		ret_num = getline(&input, &size, stdin);
-		++command_count;
-		if (ret_num == -1)
-		{
-			free(input);
-			input = NULL;
-			if (isatty(STDIN_FILENO))
-				printf("\n");
-			exit(EXIT_FAILURE);
-		}
-		input[ret_num - 1] = '\0';
+		input = get_input();
 		if (*input == '\0')
 			continue;
 		tok_ret = tokenize(input, " ");
@@ -32,11 +17,12 @@ int main(void)
 			continue;
 		if (access(tok_ret[0], X_OK) == -1)
 		{
+			exit_func(tok_ret);
 			get_pathname(&tok_ret[0]);
 			if (tok_ret != NULL && access(tok_ret[0], X_OK) == -1)
 			{
-				exit_func(tok_ret, command_count);
-				env_func(tok_ret, command_count);
+				perror("$");
+				free_commands(tok_ret);
 				continue;
 			}
 		}
