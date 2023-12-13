9945cd842e05f8a24c18785e6c56c05b8622ebb1
akoredera <akoredera@gmail.com>
2023-12-13 11:48:30 -0800

    modified _exit file

--- _exit.c
+++ _exit.c
@@ -6,9 +6,16 @@
  */
 void exit_func(char **tok_ret)
 {
+	int status = 0;
 	if (strcmp(tok_ret[0], "exit") == 0)
 	{
+		if (tok_ret[1] != NULL)
+		{
+			status = atoi(tok_ret[1]);
+			free_commands(tok_ret);
+			exit(status);
+		}
 		free_commands(tok_ret);
-		exit(0);
+		exit(status);
 	}
 }
